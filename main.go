42b30e360748984dea8e48affa11fcdf8825bf48
Adam Hintz <adamh.zero@gmail.com>
2017-09-06 20:40:29 -0700

    Style: add some formatting
    Part of #2

--- main.go
+++ main.go
@@ -16,8 +16,6 @@ import (
 
 var tpl *template.Template
 
-const wrapper = "<html><head></head><body>{{.}}</body></html>"
-
 func main() {
 	tpl, err := template.New("T").Parse(wrapper)
 	if err != nil {
@@ -42,10 +40,64 @@ func main() {
 		}
 
 		htmlResult := template.HTML(bluemonday.UGCPolicy().SanitizeBytes(blackfriday.MarkdownCommon(markdown)))
-		tpl.Execute(w, htmlResult)
+		tpl.Execute(w, struct {
+			Css      template.CSS
+			Content  template.HTML
+			FileName string
+		}{
+			Css:      template.CSS(css),
+			Content:  htmlResult,
+			FileName: path,
+		})
 	})
 
 	http.Handle("/", r)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
+const wrapper = `
+<html>
+<head>
+  <style type="text/css">
+    {{.Css}}
+  </style>
+</head>
+<body>
+	<div id="main">
+		<h1 class="file-name">{{.FileName}}</h1>
+		<div id="content">
+			{{.Content}}
+		</div>
+	</div>
+</body>
+</html>
+`
+
+const css = `
+#main {
+	width: 800px;
+	margin: 0 auto;
+	font-family: sans-serif;
+}
+
+.file-name {
+	margin-bottom: 30px;
+}
+
+#content {
+	border: 1px #DDD solid;
+}
+
+code {
+	font-family: Inconsolata, monospace;
+	padding: 0 3px;
+}
+
+pre {
+	background: #DDD;
+	border: 1px #888 solid;
+	margin: 5px;
+	padding: 5px;
+}
+`
