7b8c64c5131a4b8256d0bac91ef68aad02546a8d
Adam Hintz <adamh.zero@gmail.com>
2017-09-06 19:47:40 -0700

    Add easy cross-compilation (for OS / arch that I might care about)

---
+++ cross-compile.bash
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+PROGNAME=md-file-viewer
+mkdir -p builds
+
+function do_build()
+{
+    echo "Building $PROGNAME-$GOOS-$GOARCH"
+    go build
+    OUTPROGNAME=$PROGNAME
+    OUTGOARCH=$GOARCH
+    if [[ "$GOOS" == "windows" ]]; then
+        OUTPROGNAME=$OUTPROGNAME.exe
+        OUTGOARCH=$OUTGOARCH.exe
+    fi
+    mv $OUTPROGNAME builds/$PROGNAME-$GOOS-$OUTGOARCH
+}
+
+function do_arch_builds()
+{
+GOARCH=amd64 do_build
+GOARCH=386 do_build
+}
+
+GOOS=linux do_arch_builds
+GOOS=darwin do_arch_builds
+GOOS=windows do_arch_builds
+
+
